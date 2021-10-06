4e42182c6b7c72e20298004f9ec9fadb2ee9d3ed
Lujaina Walid <90357532+Lujaina21@users.noreply.github.com>
2021-10-06 03:47:29 +0200

    Update script.js

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,1 +1,26 @@
-// Start java script  
+'use strict'
+
+//sticky navbar
+var nav = document.getElementById('nav');
+     window.onscroll = function () {
+    if(window.pageYOffset > 100) { 
+      nav.classList.add("shrink");
+    }else{
+            nav.classList.remove("shrink");
+         }
+      }
+
+      //accordion part
+      var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
