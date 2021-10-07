dfcf29f5af45b4b86238a97addce1b52c1579af4
Abdallh Mostafa elRabiey <81104114+abdallhmostafa@users.noreply.github.com>
2021-10-07 19:05:37 +0200

    Add files via upload

---
+++ assets/Js/about.js
@@ -0,0 +1,96 @@
+// <!-- ============================= Start Section Complete area in About =================== -->
+
+let comp_rotate_one = document.getElementById("comp_rotate_one");
+let comp_text_h_one = document.getElementById("comp_text_h_one");
+let comp_text_p_one = document.getElementById("comp_text_p_one");
+
+function comp_setchange_one() {
+   comp_rotate_one.style.transform = "rotateY(360deg)"; 
+   comp_rotate_one.style.background ="#fff";
+   comp_rotate_one.style.color ="red";
+
+    comp_text_h_one.style.color="#fff";
+    comp_text_p_one.style.color="#fff";
+
+    
+}
+function comp_removechange_one(){
+     comp_rotate_one.style.transform = "rotateY(0deg)"; 
+     comp_rotate_one.style.background ="linear-gradient(to bottom right,#d2044d,#ff5e68)";
+     comp_rotate_one.style.color ="#fff";
+
+    comp_text_h_one.style.color="#000";
+    comp_text_p_one.style.color="#000";
+}
+
+let comp_rotate_two = document.getElementById("comp_rotate_two");
+let comp_text_h_two = document.getElementById("comp_text_h_two");
+let comp_text_p_two = document.getElementById("comp_text_p_two");
+
+function comp_setchange_two() {
+   comp_rotate_two.style.transform = "rotateY(360deg)"; 
+   comp_rotate_two.style.background ="#fff";
+   comp_rotate_two.style.color ="red";
+
+    comp_text_h_two.style.color="#fff";
+    comp_text_p_two.style.color="#fff";
+
+    
+}
+function comp_removechange_two(){
+     comp_rotate_two.style.transform = "rotateY(0deg)"; 
+     comp_rotate_two.style.background ="linear-gradient(to bottom right,#d2044d,#ff5e68)";
+     comp_rotate_two.style.color ="#fff";
+
+    comp_text_h_two.style.color="#000";
+    comp_text_p_two.style.color="#000";
+}
+
+let comp_rotate_three = document.getElementById("comp_rotate_three");
+let comp_text_h_three = document.getElementById("comp_text_h_three");
+let comp_text_p_three = document.getElementById("comp_text_p_three");
+
+function comp_setchange_three() {
+   comp_rotate_three.style.transform = "rotateY(360deg)"; 
+   comp_rotate_three.style.background ="#fff";
+   comp_rotate_three.style.color ="red";
+
+    comp_text_h_three.style.color="#fff";
+    comp_text_p_three.style.color="#fff";
+
+    
+}
+function comp_removechange_three(){
+     comp_rotate_three.style.transform = "rotateY(0deg)"; 
+     comp_rotate_three.style.background ="linear-gradient(to bottom right,#d2044d,#ff5e68)";
+     comp_rotate_three.style.color ="#fff";
+
+    comp_text_h_three.style.color="#000";
+    comp_text_p_three.style.color="#000";
+}
+
+let comp_rotate_four = document.getElementById("comp_rotate_four");
+let comp_text_h_four = document.getElementById("comp_text_h_four");
+let comp_text_p_four = document.getElementById("comp_text_p_four");
+
+function comp_setchange_four() {
+   comp_rotate_four.style.transform = "rotateY(360deg)"; 
+   comp_rotate_four.style.background ="#fff";
+   comp_rotate_four.style.color ="red";
+
+    comp_text_h_four.style.color="#fff";
+    comp_text_p_four.style.color="#fff";
+
+    
+}
+function comp_removechange_four(){
+     comp_rotate_four.style.transform = "rotateY(0deg)"; 
+     comp_rotate_four.style.background ="linear-gradient(to bottom right,#d2044d,#ff5e68)";
+     comp_rotate_four.style.color ="#fff";
+
+    comp_text_h_four.style.color="#000";
+    comp_text_p_four.style.color="#000";
+}
+
+
+// <!-- ============================= Ebd Section Complete area in About =================== -->
