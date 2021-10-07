6bedf0765fecf619dc40d167da46b3d6b592f55d
Abdallh Mostafa elRabiey <81104114+abdallhmostafa@users.noreply.github.com>
2021-10-07 19:03:16 +0200

    Add files via upload

---
+++ assets/Js/shop.js
@@ -0,0 +1,83 @@
+// <!-- ==================== Start Product Section  in About =============================== -->
+
+ 
+let shop_add_btn_one = document.getElementById("shop_add_btn_one");
+
+function shop_setchange_one(){
+   shop_add_btn_one.style.opacity ="1";
+   shop_add_btn_one.style.transform ="translateY(6px)";
+}
+function shop_removechange_one(){
+   shop_add_btn_one.style.opacity ="0";
+   shop_add_btn_one.style.transform ="translateY(66px)";
+
+}   
+
+
+let shop_add_btn_two = document.getElementById("shop_add_btn_two");
+
+function shop_setchange_two(){
+   shop_add_btn_two.style.opacity ="1";
+   shop_add_btn_two.style.transform ="translateY(6px)";
+}
+function shop_removechange_two(){
+   shop_add_btn_two.style.opacity ="0";
+   shop_add_btn_two.style.transform ="translateY(66px)";
+
+} 
+
+let shop_add_btn_three = document.getElementById("shop_add_btn_three");
+
+function shop_setchange_three(){
+   shop_add_btn_three.style.opacity ="1";
+   shop_add_btn_three.style.transform ="translateY(6px)";
+}
+function shop_removechange_three(){
+   shop_add_btn_three.style.opacity ="0";
+   shop_add_btn_three.style.transform ="translateY(66px)";
+
+} 
+
+
+let shop_add_btn_four = document.getElementById("shop_add_btn_four");
+
+function shop_setchange_four(){
+   shop_add_btn_four.style.opacity ="1";
+   shop_add_btn_four.style.transform ="translateY(6px)";
+}
+function shop_removechange_four(){
+   shop_add_btn_four.style.opacity ="0";
+   shop_add_btn_four.style.transform ="translateY(66px)";
+
+} 
+
+
+let shop_add_btn_five = document.getElementById("shop_add_btn_five");
+
+function shop_setchange_five(){
+   shop_add_btn_five.style.opacity ="1";
+   shop_add_btn_five.style.transform ="translateY(6px)";
+}
+function shop_removechange_five(){
+   shop_add_btn_five.style.opacity ="0";
+   shop_add_btn_five.style.transform ="translateY(66px)";
+
+} 
+
+
+
+let shop_add_btn_six = document.getElementById("shop_add_btn_six");
+
+function shop_setchange_six(){
+   shop_add_btn_six.style.opacity ="1";
+   shop_add_btn_six.style.transform ="translateY(6px)";
+}
+function shop_removechange_six(){
+   shop_add_btn_six.style.opacity ="0";
+   shop_add_btn_six.style.transform ="translateY(66px)";
+
+} 
+
+
+
+// <!-- ==================== End Product Section  in About =============================== -->
